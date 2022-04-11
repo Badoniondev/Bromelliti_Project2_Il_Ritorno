2ed82a6c3c7a42b22959a99985695a5123330fff
Badoniondev <55188452+Badoniondev@users.noreply.github.com>
2022-04-11 17:09:11 +0200

    Add files via upload

---
+++ odysseus/src/main/java/com/bromelliti/odysseus/Giocatore.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bromelliti.odysseus;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Neri
+ */
+public class Giocatore {
+    
+    Vector Inventario; //Per ora non usato
+    
+    
+    
+}
