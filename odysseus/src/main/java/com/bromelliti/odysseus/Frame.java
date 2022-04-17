e3f42ffec2a8bf563d7c58a445da29991e112010
Badonion <alex1@192.168.1.2>
2022-04-17 22:20:35 +0200

    IL FINE

--- odysseus/src/main/java/com/bromelliti/odysseus/Frame.java
+++ odysseus/src/main/java/com/bromelliti/odysseus/Frame.java
@@ -12,12 +12,15 @@ public class Frame extends javax.swing.JFrame {
 
    Ugo giocatore;
    Ugo nemico;
+   int n, n1;
+   short MORTE=0; // -1 se nemico morto, 0 se nessuno, 1 se giocatore
     
     public Frame() {
         initComponents();
         setVisibleAll(false);
         giocatore = new Ugo(true, 100, 100, 100);
         nemico = new Ugo(false, 100, 100, 100);
+        jLabelSP.setVisible(false);
     }
     
     
@@ -28,13 +31,13 @@ public class Frame extends javax.swing.JFrame {
         jButtonRicarica.setVisible(visible);
         jLabelCHP.setVisible(visible);
         jLabelCMP.setVisible(visible);
-        jLabelCSP.setVisible(visible);
+        //jLabelCSP.setVisible(visible);
         jLabelHP.setVisible(visible);
-        jLabelSP.setVisible(visible);
+        //jLabelSP.setVisible(visible);
         jLabelMP.setVisible(visible);
         jLabelEHP.setVisible(visible);
         jLabelEMP.setVisible(visible);
-        jLabelESP.setVisible(visible);
+        //jLabelESP.setVisible(visible);
     }
 
     /**
@@ -63,13 +66,30 @@ public class Frame extends javax.swing.JFrame {
         jLabelEvent2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(500, 300));
+        setMinimumSize(new java.awt.Dimension(500, 300));
         setResizable(false);
 
         jButtonRicarica.setText("Ricarica");
+        jButtonRicarica.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonRicaricaActionPerformed(evt);
+            }
+        });
 
         jButtonAttacca.setText("Attacca");
+        jButtonAttacca.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonAttaccaActionPerformed(evt);
+            }
+        });
 
         jButtonCura.setText("Cura");
+        jButtonCura.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCuraActionPerformed(evt);
+            }
+        });
 
         jLabelMP.setForeground(new java.awt.Color(0, 0, 255));
         jLabelMP.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -110,8 +130,14 @@ public class Frame extends javax.swing.JFrame {
         jLabelEMP.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
         jLabelEvent1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelEvent1.setMaximumSize(new java.awt.Dimension(34, 15));
+        jLabelEvent1.setMinimumSize(new java.awt.Dimension(34, 15));
+        jLabelEvent1.setPreferredSize(new java.awt.Dimension(34, 15));
 
         jLabelEvent2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelEvent2.setMaximumSize(new java.awt.Dimension(34, 15));
+        jLabelEvent2.setMinimumSize(new java.awt.Dimension(34, 15));
+        jLabelEvent2.setPreferredSize(new java.awt.Dimension(34, 15));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -122,22 +148,20 @@ public class Frame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabelEvent1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabelCHP, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(138, 138, 138)
-                                .addComponent(jLabelCSP, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jLabelHP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jButtonAttacca, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jLabelEHP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addGap(138, 138, 138)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jButtonInizia, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
-                                    .addComponent(jLabelSP, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
-                                    .addComponent(jButtonCura, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jLabelESP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jLabelCHP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabelHP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButtonAttacca, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabelEHP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGap(138, 138, 138)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jButtonInizia, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                                .addComponent(jLabelSP, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                                .addComponent(jButtonCura, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabelESP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabelCSP, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 108, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -157,17 +181,18 @@ public class Frame extends javax.swing.JFrame {
                     .addComponent(jLabelHP)
                     .addComponent(jLabelSP))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelEHP, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabelEMP, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabelESP, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabelESP, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabelEHP, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabelEMP, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(30, 30, 30)
                 .addComponent(jButtonInizia)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
-                .addComponent(jLabelEvent2)
+                .addComponent(jLabelEvent2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabelEvent1, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelEvent1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabelCHP, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabelCSP, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -183,16 +208,100 @@ public class Frame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void Atrocisofferenze(short M){
+        jButtonAttacca.setEnabled(false);
+        jButtonCura.setEnabled(false);
+        jButtonRicarica.setEnabled(false);
+        switch (M){
+            case 1:
+                jLabelEvent1.setText("Hai perso!");
+                break;
+            case -1:
+                jLabelEvent2.setText("Hai sconfitto Bob!");
+                break;
+        }
+    }
+    
+    private void Turnonemico(){
+        if (nemico.HP <=0 || nemico.MP <=0){
+            MORTE = -1;
+            Atrocisofferenze(MORTE);
+        }
+        if (MORTE==0) {
+            nemico.check();
+            switch (nemico.getAzionenemico()) {
+                case 1: //Nemico attacca
+                    giocatore.HP -= nemico.getAA();
+                    jLabelCHP.setText(Integer.toString(giocatore.HP));
+                    jLabelEvent1.setText("Bob ti infligge " + nemico.getAA() + " danno!");
+                    break;
+                case 2: //Nemico si cura
+                    jLabelEHP.setText(Integer.toString(nemico.HP));
+                    jLabelEMP.setText(Integer.toString(nemico.MP));
+                    jLabelEvent1.setText("Bob utilizza " + nemico.getMU() + " MP per curare " + nemico.getHA() + " HP!");
+                    break;
+                case 3: //Nemico ruba MP al giocatore
+                    giocatore.MP -= nemico.getMS();
+                    jLabelEMP.setText(Integer.toString(nemico.MP));
+                    jLabelCMP.setText(Integer.toString(giocatore.MP));
+                    jLabelEvent1.setText("Bob ti ruba " + nemico.getMS() + " MP!");
+                    break;
+            }
+        }
+        if (giocatore.HP <=0 || giocatore.MP <=0){
+            MORTE = 1;
+            Atrocisofferenze(MORTE);
+        }
+    }
+    
     private void Inizia(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Inizia
         setVisibleAll(true);
         jButtonInizia.setVisible(false);
         jLabelCHP.setText(Integer.toString(giocatore.getHPMAX()));
-        jLabelCSP.setText(Integer.toString(giocatore.getSPMAX()));
+        //jLabelCSP.setText(Integer.toString(giocatore.getSPMAX()));
         jLabelCMP.setText(Integer.toString(giocatore.getMPMAX()));
         jLabelEHP.setText(Integer.toString(nemico.getHPMAX()));
-        jLabelESP.setText(Integer.toString(nemico.getSPMAX()));
+        //jLabelESP.setText(Integer.toString(nemico.getSPMAX()));
         jLabelEMP.setText(Integer.toString(nemico.getMPMAX()));
+        jLabelEvent1.setText("Sconfiggi Bob!");
     }//GEN-LAST:event_Inizia
+
+    private void jButtonAttaccaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAttaccaActionPerformed
+        n = giocatore.attack();
+        nemico.HP-=n;
+        jLabelEHP.setText(Integer.toString(nemico.HP));
+        jLabelEvent2.setText("Attacchi Bob per " + n + " danno!");
+        Turnonemico();
+    }//GEN-LAST:event_jButtonAttaccaActionPerformed
+
+    private void jButtonCuraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCuraActionPerformed
+        giocatore.heal();
+        n = giocatore.getHA();
+        n1 = giocatore.getMU();
+        if (giocatore.HP > 100){
+            giocatore.HP = 100;
+            jLabelCHP.setText("100");
+        } else {
+            jLabelCHP.setText(Integer.toString(giocatore.HP));
+        }
+        jLabelCMP.setText(Integer.toString(giocatore.MP));
+        jLabelEvent2.setText("Usi " + n1 + " MP e ti curi per " + n + " HP!");
+        Turnonemico();
+    }//GEN-LAST:event_jButtonCuraActionPerformed
+
+    private void jButtonRicaricaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonRicaricaActionPerformed
+        giocatore.recharge();
+        n = giocatore.getMG();
+        if(giocatore.MP > 100){
+            giocatore.MP = 100;
+            jLabelCMP.setText("100");
+        } else {
+            jLabelCMP.setText(Integer.toString(giocatore.MP));
+        }
+        jLabelCMP.setText(Integer.toString(giocatore.MP));
+        jLabelEvent2.setText("Ricarichi " + giocatore.getMG() + " MP!");
+        Turnonemico();
+    }//GEN-LAST:event_jButtonRicaricaActionPerformed
 
     /**
      * @param args the command line arguments
